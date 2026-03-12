5262de57118253e68015a5e9f9bed66697876674
Ghost91000 <47149483+Ghost91000@users.noreply.github.com>
2026-03-12 17:11:23 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,15 +4,15 @@ from functools import lru_cache
 
 class Settings(BaseSettings):
 
-    PRIVATE_KEY_HEX: str = "f0aadfc81b9c6383ccb597d0fd8bfc1a956d35022a36c3d0b7531814b5e54e73"
-    SECRET_KEY: str = "gV64m9aIzFG4qpgVphvQbPQrtAO0nM-7YwwOvu0XPt5KJOjAy4AfgLkqJXYEt"
+    PRIVATE_KEY_HEX: str = "key"
+    SECRET_KEY: str = "key"
     DEBUG: bool = True
     ALGORITHM: str = "HS256"
     COOKIE_NAME: str = "access_token"
     REFRESH_COOKIE_NAME: str = "refresh_token"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 15
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
-    TG_TOKEN: str = "7106874206:AAGEbotJoabRmsngGgsxX-TwZ04MpoJiYQ0"
+    TG_TOKEN: str = "tg-token"
 
     DB_USER: str = "postgres"
     DB_PASSWORD: str = "12345678"
